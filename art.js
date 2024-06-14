67d3a5a48989e7ac5509401be4157db79e021c3a
haec314 <rbfldbfl123@naver.com>
2024-06-14 14:51:23 +0900

    완성

--- art.js
+++ art.js
@@ -1,48 +1,51 @@
-//백지
-const canvas = document.getElementById("drawing");
-const ctx = canvas.getContext("2d");
+window.onload = function() {
 
-canvas.width=400;
-canvas.height=400;
+    //백지
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
-ctx.strokeStyle = "purple";
-ctx.lineWidth=20;
-ctx.lineCap="round";
+    canvas.width=400;  
+    canvas.height=400;
+
+    ctx.strokeStyle = "purple";
+    ctx.lineWidth=20;
+    ctx.lineCap="round";
 
 
-//마우스
-let painting=false;
+    //마우스
+    let painting=false;
 
-function stopPainting(){
-    painting = false;
-    ctx.closePath();
+    function stopPainting(){
+        painting = false;
+        ctx.closePath();
 
-}
-
-function startPainting(event){
-    painting=true;
-
-    const x = event.offsetX;
-    const y = event.offsetY
-    ctx.beginPath(); //초기화
-    ctx.moveTo(x,y);
-}
-
-function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY
-
-    if (!painting){
-        return;
     }
-    ctx.lineTo(x,y);
-    ctx.stroke();
-}
 
-if(canvas){
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown",startPainting);
-    canvas.addEventListener("mouseup",stopPainting);
-    canvas.addEventListener("mouseleave",stopPainting);
-}
+    function startPainting(event){
+        painting=true;
 
+        const x = event.offsetX;
+        const y = event.offsetY
+        ctx.beginPath(); //초기화
+        ctx.moveTo(x,y);
+    }
+
+    function onMouseMove(event){
+        const x = event.offsetX;
+        const y = event.offsetY
+
+        if (!painting){
+            return;
+        }
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+
+    if(canvas){
+        canvas.addEventListener("mousemove", onMouseMove);
+        canvas.addEventListener("mousedown",startPainting);
+        canvas.addEventListener("mouseup",stopPainting);
+        canvas.addEventListener("mouseleave",stopPainting);
+    }
+
+}
